7975f2778f6198cce7b4e50148bd4034e1511f98
Anica Djukic <anica.djukic@Praksa1O.vega.local>
2022-06-27 10:52:56 +0200

    initial project

---
+++ ExpenseTracker/src/test/java/com/expense/ExpenseTracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expense.ExpenseTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
