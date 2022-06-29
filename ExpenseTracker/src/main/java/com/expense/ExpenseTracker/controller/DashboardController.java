81283f4522a3594edb7385c416221932f469d132
Anica Djukic <anica.djukic@Praksa1O.vega.local>
2022-06-29 17:20:05 +0200

    feat: get dashboard

---
+++ ExpenseTracker/src/main/java/com/expense/ExpenseTracker/controller/DashboardController.java
@@ -0,0 +1,47 @@
+package com.expense.ExpenseTracker.controller;
+
+import com.expense.ExpenseTracker.dto.DashboardDto;
+import com.expense.ExpenseTracker.dto.ExpenseResponseDto;
+import com.expense.ExpenseTracker.dto.IncomeResponseDto;
+import com.expense.ExpenseTracker.model.Expense;
+import com.expense.ExpenseTracker.model.Income;
+import com.expense.ExpenseTracker.service.DashboardService;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/dashboard")
+public class DashboardController {
+
+    private final DashboardService dashboardService;
+
+    private final ModelMapper modelMapper = new ModelMapper();
+
+    public DashboardController(DashboardService dashboardService) {
+        this.dashboardService = dashboardService;
+    }
+
+    @GetMapping
+    public ResponseEntity<DashboardDto> getDashboard() {
+        double totalAmount = dashboardService.getTotalAmount();
+        List<Expense> expenses = dashboardService.getLastFiveExpenses();
+        List<Income> incomes = dashboardService.getLastFiveIncomes();
+        return ResponseEntity.ok(createDashboard(totalAmount, expenses, incomes));
+    }
+
+    private DashboardDto createDashboard(double totalAmount, List<Expense> expenses, List<Income> incomes) {
+        List<ExpenseResponseDto> expenseDtos = new ArrayList<>();
+        for (Expense expense : expenses)
+            expenseDtos.add(modelMapper.map(expense, ExpenseResponseDto.class));
+        List<IncomeResponseDto> incomeDtos = new ArrayList<>();
+        for(Income income : incomes)
+            incomeDtos.add(modelMapper.map(income, IncomeResponseDto.class));
+        return new DashboardDto(totalAmount, expenseDtos, incomeDtos);
+    }
+}
