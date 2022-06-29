5cbdc20cbff54c73467a1bab0a924dd15c9c9056
Anica Djukic <anica.djukic@Praksa1O.vega.local>
2022-06-29 12:59:47 +0200

    feat: try to implement getting expenses by Querydsl

--- ExpenseTracker/src/main/java/com/expense/ExpenseTracker/repository/ExpenseRepository.java
+++ ExpenseTracker/src/main/java/com/expense/ExpenseTracker/repository/ExpenseRepository.java
@@ -3,10 +3,11 @@ package com.expense.ExpenseTracker.repository;
 import com.expense.ExpenseTracker.model.Expense;
 import com.expense.ExpenseTracker.model.ExpenseGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface ExpenseRepository extends JpaRepository<Expense, UUID> {
+public interface ExpenseRepository extends JpaRepository<Expense, UUID>, QuerydslPredicateExecutor<Expense> {
     List<Expense> findByExpenseGroupOrderByCreationTimeDesc(ExpenseGroup expenseGroup);
 }
--- ExpenseTracker/src/main/java/com/expense/ExpenseTracker/service/ExpenseService.java
+++ ExpenseTracker/src/main/java/com/expense/ExpenseTracker/service/ExpenseService.java
@@ -3,9 +3,14 @@ package com.expense.ExpenseTracker.service;
 import com.expense.ExpenseTracker.dto.ExpenseRequestDto;
 import com.expense.ExpenseTracker.exception.NotFoundException;
 import com.expense.ExpenseTracker.model.Expense;
+import com.expense.ExpenseTracker.model.QExpense;
 import com.expense.ExpenseTracker.repository.ExpenseRepository;
+import com.querydsl.jpa.impl.JPAQuery;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -17,9 +22,14 @@ public class ExpenseService {
 
     private final ExpenseGroupService expenseGroupService;
 
-    public ExpenseService(ExpenseRepository repository, ExpenseGroupService expenseGroupService) {
+    private EntityManager entityManager;
+
+    private final ModelMapper modelMapper = new ModelMapper();
+
+    public ExpenseService(ExpenseRepository repository, ExpenseGroupService expenseGroupService, EntityManager entityManager) {
         this.repository = repository;
         this.expenseGroupService = expenseGroupService;
+        this.entityManager = entityManager;
     }
 
     public Expense addNew(Expense expense, UUID expenseGroupId) throws NotFoundException {
@@ -50,6 +60,13 @@ public class ExpenseService {
     }
 
     public List<Expense> getByExpenseGroupId(UUID expenseGroupId) {
-        return repository.findByExpenseGroupOrderByCreationTimeDesc(expenseGroupService.getById(expenseGroupId));
+        QExpense expense = QExpense.expense;
+        JPAQuery<QExpense> query = new JPAQuery<>(entityManager);
+        query.from(expense).where(expense.expenseGroup.id.eq(expenseGroupId));
+        List<Expense> expenses = new ArrayList<>();
+        for (int i = 0; i < query.fetch().size(); i++) {
+            expenses.add(modelMapper.map(query.fetch().get(i), Expense.class));
+        }
+        return expenses;
     }
 }
