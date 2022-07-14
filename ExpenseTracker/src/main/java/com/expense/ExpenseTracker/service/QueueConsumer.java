937675ab56bb3deb8d77dc8926e56bdf78626825
Anica Djukic <anica.djukic@Praksa1O.vega.local>
2022-07-14 14:04:03 +0200

    feat: add new expense group by rabbit mq

--- ExpenseTracker/src/main/java/com/expense/ExpenseTracker/service/QueueConsumer.java
+++ ExpenseTracker/src/main/java/com/expense/ExpenseTracker/service/QueueConsumer.java
@@ -1,16 +1,38 @@
 package com.expense.ExpenseTracker.service;
 
+import com.expense.ExpenseTracker.model.ExpenseGroup;
+import com.expense.ExpenseTracker.model.User;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 
 @Component
 public class QueueConsumer {
 
-    @RabbitListener(queues = {"${queue.name}"})
-    public void receive(@Payload String fileBody) {
+    private final UserService userService;
+
+    private final ExpenseGroupService expenseGroupService;
+
+    public QueueConsumer(UserService userService, ExpenseGroupService expenseGroupService) {
+        this.userService = userService;
+        this.expenseGroupService = expenseGroupService;
+    }
+
+    @RabbitListener(queues = {"expense-groups"})
+    public void receiveExpenseGroup(@Payload String fileBody) {
         System.out.println("Message " + fileBody);
+        JsonObject convertedObject = new Gson().fromJson(fileBody, JsonObject.class);
+        System.out.println(convertedObject.toString());
+
+        ExpenseGroup expenseGroup = new ExpenseGroup(convertedObject.get("name").getAsString(), convertedObject.get("description").getAsString());
+        User user = userService.getById(UUID.fromString(convertedObject.get("userId").getAsString()));
+        ExpenseGroup savedExpenseGroup = expenseGroupService.addNew(expenseGroup, user.getUsername());
+        System.out.println(savedExpenseGroup);
     }
 
 }
