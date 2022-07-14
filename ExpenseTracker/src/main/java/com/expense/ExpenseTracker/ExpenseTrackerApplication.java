ceed387990a47755d89e20a5944a19e3ffbec3d6
Anica Djukic <anica.djukic@Praksa1O.vega.local>
2022-07-14 12:17:39 +0200

    feat: rabbit mq started

--- ExpenseTracker/src/main/java/com/expense/ExpenseTracker/ExpenseTrackerApplication.java
+++ ExpenseTracker/src/main/java/com/expense/ExpenseTracker/ExpenseTrackerApplication.java
@@ -1,9 +1,11 @@
 package com.expense.ExpenseTracker;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableRabbit
 @SpringBootApplication
 @EnableScheduling
 public class ExpenseTrackerApplication {
---
+++ ExpenseTracker/src/main/java/com/expense/ExpenseTracker/service/QueueConsumer.java
@@ -0,0 +1,16 @@
+package com.expense.ExpenseTracker.service;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class QueueConsumer {
+
+    @RabbitListener(queues = {"${queue.name}"})
+    public void receive(@Payload String fileBody) {
+        System.out.println("Message " + fileBody);
+    }
+
+}
